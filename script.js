a575a51336c71f27926acc156d22fc82adf8ffc3
Adam <adammiller.2000nj@gmail.com>
2023-03-19 15:34:56 -0400

    added board/title/background/font

---
+++ script.js
@@ -0,0 +1,12 @@
+let board = document.getElementById("board");
+
+function createBoard(row, col) {
+    board.style.setProperty('--slot-rows', row);
+    board.style.setProperty('--slot-cols', col);
+    for (b = 0; b < (row * col); b++) {
+        let slot = document.createElement("div");
+        slot.id = "slot-" + b;
+        board.appendChild(slot).className = 'square'
+    }
+}
+createBoard(32, 32);
